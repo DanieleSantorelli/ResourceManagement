69b3e129ba9f3d8929d1f89fa191145e0b9b1609
JD3V <mynam3isjack@users.noreply.github.com>
2021-06-29 15:19:43 +0200

    backup database

---
+++ Database/Sql/Pulisce_database.sql
@@ -0,0 +1,10 @@
+delete from Capacities
+delete from Iterations
+delete from DaysOff
+delete from Activities
+delete from Members
+delete from Members_Teams
+delete from teams
+delete from Project_AreaPath
+delete from RootClassificationNodes
+delete from Projects
